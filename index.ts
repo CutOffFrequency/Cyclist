d5d0f066ce8734f3780d1dd7f78f68b98015912f
Erik <erik@gnar.dog>
2020-10-04 22:12:24 -0400

    Adds method to add a node at specified index
    This commit adds the method insertAtIndex which takes the data for a new node and index, iterates through the list and inserts the node inbetween extant nodes at the specified index
    
    A comment was also added to explain the inclusion of the taill property on the list which may seem counter intuitive given that the head's previous is the tail in a cyclical linked list
    
    Also included is the method removeAtIndex, currently this work only contains edge cases and the remaining logic has yet to be added

--- index.ts
+++ index.ts
@@ -19,6 +19,9 @@ export interface LinkedList<T> {
   size: number
 }
 
+// Keeping track of both a head and tail for a cyclical doubly linked list may seem redundant
+// since the tail is just the current head's previous node however;
+// when adding new nodes at the head we don't need to iterate through the entire list
 export class LinkedList<T> {
   constructor(args: LinkedList<T>) {
     const { head, tail, size } = args
@@ -33,14 +36,17 @@ export class LinkedList<T> {
       next: () => previousHead,
       previous: () => this.tail,
     })
+
     if (this.head) {
       this.head.previous = () => newNode
     }
+
     this.head = newNode
     if (!this.tail) {
       this.tail = newNode
     }
-    this.size += 1
+
+    return (this.size += 1)
   }
 
   insertAtTail(data: T) {
@@ -50,13 +56,72 @@ export class LinkedList<T> {
       next: () => this.head,
       previous: () => previousTail,
     })
+
     if (this.tail) {
       this.tail.next = () => newNode
     }
+
     this.tail = newNode
     if (!this.head) {
       this.head = newNode
     }
-    this.size += 1
+
+    return (this.size += 1)
+  }
+
+  insertAtIndex(data: T, index: number) {
+    if (index === 0) {
+      this.insertAtHead(data)
+    }
+
+    if (index >= this.size) {
+      this.insertAtTail(data)
+    }
+
+    let currentNode = this.head
+    let previousNode
+    let nextNode
+    let count = 0
+    const insertNewNode = () => {
+      const newNode = new Node({
+        data,
+        next: () => previousNode.next,
+        previous: () => previousNode,
+      })
+
+      previousNode.next = () => newNode
+      newNode.next = () => nextNode
+      return (this.size += 1)
+    }
+
+    while (count < index) {
+      if (count === index - 1) {
+        previousNode = currentNode
+        nextNode = currentNode.next()
+      }
+
+      if (count === index) {
+        return insertNewNode()
+      }
+
+      currentNode = currentNode.next()
+      count += 1
+    }
+  }
+
+  removeAtIndex(index: number) {
+    if (index === 0) {
+      const previousHead = this.head
+      this.head = previousHead.next()
+      this.tail.next = () => this.head
+      return (this.size -= 1)
+    }
+
+    if (index === this.size - 1) {
+      const previousTail = this.tail
+      this.tail = previousTail.previous()
+      this.tail.next = () => this.head
+      return (this.size -= 1)
+    }
   }
 }
